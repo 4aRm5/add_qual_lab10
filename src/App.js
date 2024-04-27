e85ac4e51c2cc33e3e27a835a1d7d76d36fd5ca6
4aRm5 <134143170+4aRm5@users.noreply.github.com>
2024-04-27 18:17:25 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import ToDo from './components/ToDo';
+import img1 from "./images/f3.png";
+import img2 from "./images/f4.png";
+import img3 from "./images/f5.png";
+
+function App() {
+  return (
+    <div>
+      <h1 class="title">Что вам необходимо?</h1>
+      <div class="main__blocks">
+        <ToDo title="Первый блок" img={img1} text='Lorem, ipsum dolor sit amet consectetur adipiscing elit'/>
+        <ToDo title="Второй блок" img={img2} text='Lorem, ipsum dolor sit amet consectetur adipiscing elit'/>
+        <ToDo title="Третий блок" img={img3} text='Lorem, ipsum dolor sit amet consectetur adipiscing elit'/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
